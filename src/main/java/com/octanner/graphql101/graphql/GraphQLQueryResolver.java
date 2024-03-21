a4aa3c01a956312e8344730ac58c479fc26e31f0
Jay Wagnon <jaywagnon@gmail.com>
2024-03-21 09:42:42 -0600

    Clean up.

--- src/main/java/com/octanner/graphql101/graphql/GraphQLQueryResolver.java
+++ src/main/java/com/octanner/graphql101/graphql/GraphQLQueryResolver.java
@@ -1,13 +1,9 @@
 package com.octanner.graphql101.graphql;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-@Slf4j
 @Controller
-@RequiredArgsConstructor
 public class GraphQLQueryResolver {
 
     @QueryMapping
