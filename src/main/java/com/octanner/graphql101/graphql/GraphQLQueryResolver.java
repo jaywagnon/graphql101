299c1711acadf033241d20522d907f0782f6f7c9
Jay Wagnon <jaywagnon@gmail.com>
2024-03-21 14:55:14 -0600

    Add basic GraphQL schema and resolver

---
+++ src/main/java/com/octanner/graphql101/graphql/GraphQLQueryResolver.java
@@ -0,0 +1,13 @@
+package com.octanner.graphql101.graphql;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GraphQLQueryResolver {
+
+    @QueryMapping
+    public Boolean healthCheck() {
+        return true;
+    }
+}
