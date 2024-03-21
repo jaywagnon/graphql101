22d34055b3f30cb9f7d72f11f59b93cd6536e08d
Jay Wagnon <jaywagnon@gmail.com>
2024-03-21 10:30:45 -0600

    Remove libraries.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,33 +14,3 @@ INSERT INTO books VALUES (6, 'Little Women', 4);
 INSERT INTO books VALUES (7, 'The Invisible Man', 5);
 INSERT INTO books VALUES (8, 'The Time Machine', 5);
 INSERT INTO books VALUES (9, 'Alice''s Adventures in Wonderland & Through the Looking-Glass', 6);
-
-INSERT INTO libraries VALUES (1, 'Salt Lake City Library');
-INSERT INTO libraries VALUES (2, 'Ogden Library');
-INSERT INTO libraries VALUES (3, 'Provo Library');
-INSERT INTO libraries VALUES (4, 'Logan Library');
-
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 1);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 2);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 3);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 4);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 5);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 6);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 7);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 8);
-INSERT INTO libraries_books (library_id, book_id) VALUES (1, 9);
-
-INSERT INTO libraries_books (library_id, book_id) VALUES (2, 1);
-INSERT INTO libraries_books (library_id, book_id) VALUES (2, 5);
-INSERT INTO libraries_books (library_id, book_id) VALUES (2, 6);
-INSERT INTO libraries_books (library_id, book_id) VALUES (2, 8);
-INSERT INTO libraries_books (library_id, book_id) VALUES (2, 9);
-
-INSERT INTO libraries_books (library_id, book_id) VALUES (3, 4);
-INSERT INTO libraries_books (library_id, book_id) VALUES (3, 5);
-INSERT INTO libraries_books (library_id, book_id) VALUES (3, 6);
-INSERT INTO libraries_books (library_id, book_id) VALUES (3, 7);
-
-INSERT INTO libraries_books (library_id, book_id) VALUES (4, 3);
-INSERT INTO libraries_books (library_id, book_id) VALUES (4, 5);
-INSERT INTO libraries_books (library_id, book_id) VALUES (4, 7);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,3 @@
-CREATE TABLE libraries (
-    id int not null AUTO_INCREMENT,
-    name varchar(100) not null,
-    PRIMARY KEY ( ID )
-);
-
 CREATE TABLE authors (
     id int not null AUTO_INCREMENT,
     name varchar(100) not null,
@@ -14,12 +8,5 @@ CREATE TABLE books (
     id int not null AUTO_INCREMENT,
     name varchar(100) not null,
     author_id int not null,
-    PRIMARY KEY ( ID )
-);
-
-CREATE TABLE libraries_books (
-    id int not null AUTO_INCREMENT,
-    library_id int not null,
-    book_id int not null,
     PRIMARY KEY ( ID )
 );
