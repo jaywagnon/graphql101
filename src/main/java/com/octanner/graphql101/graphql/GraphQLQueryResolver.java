1a3ecca6f23450fcc35080450b3b51bb3921033e
Jay Wagnon <jaywagnon@gmail.com>
2024-03-21 14:44:44 -0600

    Import optimization.

--- src/main/java/com/octanner/graphql101/graphql/GraphQLQueryResolver.java
+++ src/main/java/com/octanner/graphql101/graphql/GraphQLQueryResolver.java
@@ -1,11 +1,9 @@
 package com.octanner.graphql101.graphql;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequiredArgsConstructor
 public class GraphQLQueryResolver {
 
     @QueryMapping
